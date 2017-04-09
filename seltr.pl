ac2c1dfa124afa28ebac68884da1ce386a22c009
Stefano <sinageruk@gmail.com>
2017-04-09 17:10:06 +0200

    functional release

--- seltr.pl
+++ seltr.pl
@@ -2,21 +2,39 @@
 
 use DBI;
 
-$unit = $ARGV[0];
+$encoding = $ARGV[0];
+$idlen = $ARGV[1];
+$dbloc = $ARGV[2];
+$dbtable = $ARGV[3];
+
+if ((length($encoding)==0) or (length($idlen)==0) or (length($dbloc)==0) or (length($dbtable)==0)){
+	print "SELTR: must specify encoding, number of digits, database location and lookup table name\n";
+	print "       Example:   seltr.pl CCIR 5 /home/jdoe/database it_vf\n";
+	exit;}
 
 # DB init
 $dbh = DBI->connect ("dbi:CSV:", undef, undef, {
-	f_dir => "./database",
+    f_dir => $dbloc,
     f_ext      => ".csv/r",
     RaiseError => 1,
     csv_sep_char => ";",
     }) or die "Cannot connect: $DBI::errstr";
 
-$sth = $dbh->prepare ("select UNIT from ids where (ID = ?)");
-	
+# prepare DB query
+$sth = $dbh->prepare ("select UNIT from $dbtable where (ID = ?)");
+
+
+print "-- SELTR running with: $encoding $idlen $dbloc $dbtable\n"; 
+
+# main loop
 while (<STDIN>){
-	print "$unit ";
-	$sth->execute($unit);
-	$descr = $sth->fetchrow_array();
-	print "> $descr\n";
+	chomp;
+	$unit = $_;
+	$unit =  substr $unit, (length($encoding)+2);
+	if(length($unit) >= $idlen){
+		print "$unit ";
+		$sth->execute($unit);
+		$descr = $sth->fetchrow_array();
+		print "> $descr\n";
+		}
 	}
